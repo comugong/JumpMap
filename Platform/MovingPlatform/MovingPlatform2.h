b7b4ede33665ef240b122a35ef2a93fc0aa21df7
comugong <165180439+comugong@users.noreply.github.com>
2026-08-12 18:40:08 +0900

    Add MovingPlatform2 class with movement functionality

---
+++ Platform/MovingPlatform/MovingPlatform2.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform2.generated.h"
+
+UCLASS()
+class JUMPMAP_API AMovingPlatform2 : public AActor {
+	GENERATED_BODY()
+	
+public:	
+	AMovingPlatform2();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Components")
+	USceneComponent* SceneRoot;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Components")
+	UStaticMeshComponent* StaticMeshComp;
+
+	// 이동 속도
+	float MoveSpeed;
+
+	// 이동 방향
+	FVector MoveDirection[4];
+
+	// 이동 방향 인덱스
+	int Count;
+
+	// 타이머
+	FTimerHandle TimerHandle;
+
+	virtual void BeginPlay() override;
+	void MoveLikeSquare();
+};
