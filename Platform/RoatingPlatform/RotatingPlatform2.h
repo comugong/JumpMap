89cac2a5c0f19799afe56922bd16588da949d320
comugong <165180439+comugong@users.noreply.github.com>
2026-08-12 19:27:53 +0900

    Create RotatingPlatform2.h

---
+++ Platform/RoatingPlatform/RotatingPlatform2.h
@@ -0,0 +1,24 @@
+#include "RotatingPlatform1.h"
+
+ARotatingPlatform1::ARotatingPlatform1() {
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+
+	PrimaryActorTick.bCanEverTick = true;
+	RotationSpeed = 90.0f;
+}
+
+void ARotatingPlatform1::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void ARotatingPlatform1::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+	if (!FMath::IsNearlyZero(RotationSpeed)) {
+		AddActorLocalRotation(FRotator(0.0f, RotationSpeed * DeltaTime, 0.0f));
+	}
+}
